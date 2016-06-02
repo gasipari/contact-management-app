8b02f5e4d41463a3863cb9f0c383183858474291
gasipari <riamarius@gmail.com>
2016-06-02 17:16:17 -0400

    Interim checkin - add API mongoose user model

---
+++ api/create-user.js
@@ -0,0 +1,47 @@
+// Utility to create an admin user in DB
+var path = require("path"),
+    User = require(path.join(__dirname, "models", "user.js")),
+    mongoose_uri = process.env.MONGOOSE_URI || "localhost:27017/contact-management-api";
+
+var args = process.argv.slice(2);
+
+var username = args[0];
+var password = args[1];
+
+// validate input
+if (args.length < 2) {
+    console.log("usage: node %s %s %s", path.basename(process.argv[1]), "user", "password");
+    process.exit();
+}
+
+console.log("Username: %s", username);
+console.log("Password: %s", "**only you know it**");
+
+console.log("Creating a new user in Mongo");
+
+// instantiate mongoose
+var mongoose = require("mongoose");
+mongoose.set("debug", true);
+mongoose.connect(mongoose_uri);
+mongoose.connection.on("error", function () {
+    console.log("Mongoose connection error", arguments);
+});
+mongoose.connection.once("open", function callback() {
+    console.log("Mongoose connected to the database");
+
+    // create a new user
+    var user = new User();
+
+    user.username = username;
+    user.password = password;
+
+    // save the user
+    user.save(function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(user);
+        }
+        process.exit();
+    });
+});
--- server.js
+++ server.js
@@ -1,12 +1,26 @@
-// load express lib
+// import libs
 var express = require("express");
+var bodyParser = require("body-parser");
 
-// create the app
+// create the server
 var app = express();
+
+// setup bodyParser so that we get data from POST requests
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// API routes
+
+// create instance of express router
+var router = express.Router();
+
+// set the port
 const PORT = process.env.PORT || 3000;
 
+// serving client
 app.use(express.static("public"));
 
+// start the server
 app.listen(PORT, function() {
     console.log("Express server is up on port " + PORT);
 });
