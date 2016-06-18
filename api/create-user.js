92b968216c17d2f4946116dd9c8eeebc09331b22
gasipari <riamarius@gmail.com>
2016-06-17 21:22:20 -0400

    Update .gitingore && minor refactor

--- api/create-user.js
+++ api/create-user.js
@@ -2,51 +2,58 @@
  * Utility to create an admin user in DB.
  *
  */
-var path = require("path"),
-    User = require(path.join(__dirname, "models", "user.js")),
-    mongoose_uri = process.env.MONGOOSE_URI || "localhost:27017/contact-management-api";
 
-var args = process.argv.slice(2);
+ // CONFIG ====
+ // If undefined in the process load local file
+ if(!process.env.MONGOOSE_URI) {
+     /*eslint no-unused-vars: 0*/
+     var env = require("../env.js");
+ }
+ var path = require("path"),
+     User = require(path.join(__dirname, "models", "user.js")),
+     mongoose_uri = process.env.MONGOOSE_URI;
 
-var username = args[0];
-var password = args[1];
+ var args = process.argv.slice(2);
+
+ var username = args[0];
+ var password = args[1];
 
 // validate input
-if (args.length < 2) {
-    console.log("usage: node %s %s %s", path.basename(process.argv[1]), "user", "password");
-    process.exit();
-}
+ if (args.length < 2) {
+     console.log("usage: node %s %s %s", path.basename(process.argv[1]), "user", "password");
+     process.exit();
+ }
 
-console.log("Username: %s", username);
-console.log("Password: %s", "**only you know it**");
+ console.log("Username: %s", username);
+ console.log("Password: %s", "**only you know it**");
 
-console.log("Creating a new user in Mongo");
+ console.log("Creating a new user in Mongo");
 
 // instantiate mongoose
-var mongoose = require("mongoose");
-mongoose.set("debug", true);
+ var mongoose = require("mongoose");
+ mongoose.set("debug", true);
 
 // connect to mongo
-mongoose.connect(mongoose_uri);
-mongoose.connection.on("error", function () {
-    console.log("Mongoose connection error", arguments);
-});
-mongoose.connection.once("open", function callback() {
-    console.log("Mongoose connected to the database");
+ mongoose.connect(mongoose_uri);
+ mongoose.connection.on("error", function () {
+     console.log("Mongoose connection error", arguments);
+ });
+ mongoose.connection.once("open", function callback() {
+     console.log("Mongoose connected to the database");
 
     // create a new user
-    var user = new User();
+     var user = new User();
 
-    user.username = username;
-    user.password = password;
+     user.username = username;
+     user.password = password;
 
     // save the user
-    user.save(function (err) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log(user);
-        }
-        process.exit();
-    });
-});
+     user.save(function (err) {
+         if (err) {
+             console.log(err);
+         } else {
+             console.log(user);
+         }
+         process.exit();
+     });
+ });
--- env.js
+++ env.js
@@ -1,4 +1,7 @@
-process.env["MONGOOSE_URI"] = "localhost:27017/contact-management-api";
+//process.env["MONGOOSE_URI"] = "localhost:27017/contact-management-api";
+process.env["MONGOOSE_URI"] = "mongodb://dwc:beHappy!@ds013212.mlab.com:13212/contact-management-api"
 process.env["TOKEN_EXPIRATION_SEC"] = 3600;
 process.env["PORT"] = 3000;
 process.env["ACCESS_TOKEN_SECRET"] = "***superSecret***";
+
+// mongodb://<dbuser>:<dbpassword>@ds013212.mlab.com:13212/contact-management-api
